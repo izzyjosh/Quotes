2c23cc136cb74fad74bc9e0175b038713537d3b9
izzyjosh <calebayuba222@gmail.com>
2024-01-31 18:48:42 +0100

    New project

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from .models import (Note,NoteShared)
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    empty_value_display = "--empty--"
+    list_display = ("username","email")
+    search_fields = ("username","email")
+    date_hierarchy = "date_joined"
+    search_help_text = "search by ..." 
+
+    fieldsets = (
+            (None, {"fields": ("username", "password")}),
+            (_("Personal Info"), {"fields": ("first_name", "last_name", "email",)}),
+            (
+                _("Permissions"),
+                {
+                    "fields": (
+                        "is_active",
+                        "is_staff",
+                        "is_superuser",
+                        "groups",
+                        "user_permissions",
+                        ),
+                    },
+                ),
+            (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+            )
+    add_fieldsets = (
+            (None, {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2")
+                }),
+            )
+
+admin.site.register(Note)
+admin.site.register(NoteShared)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid 
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(
+            primary_key=True,
+            default=uuid.uuid4,
+            editable=False,
+            )
+    username = models.CharField(max_length=200,unique=True)
+    email = models.EmailField()
+
+    USERNAME_FIELD = ("username")
+    REQUIRED_FIELD = ("email")
+
+    def __str__(self):
+        return self.username
+
+
+class Note(models.Model):
+    title:str = models.CharField(max_length=300)
+    audio_note = models.FileField(upload_to="audio_note",blank=True,null=True)
+    video_note = models.FileField(upload_to="video_note",blank=True,null=True)
+    content:str = models.TextField(blank=True,null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    user:User = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ("-date",)
+
+    def __str__(self):
+        return self.title
+
+
+class NoteShared(models.Model):
+    note = models.ForeignKey(Note,on_delete=models.CASCADE)
+    shared_with = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Notes Shared"
+
+    def __str__(self):
+        return self.note.title
