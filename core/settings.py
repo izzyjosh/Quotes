429623e52ff2b0fc54b60ae2934d929bb4a5bec7
izzyjosh <calebayuba222@gmail.com>
2024-01-31 23:27:30 +0100

    Configurations

--- core/settings.py
+++ core/settings.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main.apps.MainConfig",
+    "rest_framework",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
@@ -103,3 +105,10 @@ STATIC_URL = "static/"
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#rest framework settings
+
+REST_FRAMEWORK = {
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10
+        }
