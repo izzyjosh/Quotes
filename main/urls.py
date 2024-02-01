9833cb335342018018d2c206e629460be0115228
izzyjosh <calebayuba222@gmail.com>
2024-02-01 19:55:15 +0100

    Added url path for signup view

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken import views as auth_views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
         path("",views.UserList.as_view(),name="user-list"),
-        path("user_detail/<uuid:pk>/",views.UserDetail.as_view(),name="user-detail")
+        path("user_detail/<uuid:pk>/",views.UserDetail.as_view(),name="user-detail"),
+        path('api-token-auth/', auth_views.obtain_auth_token,name="obtain-token"),
+        path("signup/",views.RegisterUser.as_view(),name="signup")
         ]
 
 
