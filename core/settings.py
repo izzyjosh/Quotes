a88f83c3658ceb5216289322966d2a6600e1bc71
izzyjosh <calebayuba222@gmail.com>
2024-02-02 20:58:01 +0100

    Modifications

--- core/settings.py
+++ core/settings.py
@@ -111,7 +111,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 10
+        'PAGE_SIZE': 10,
+        "NONE_FIELD_ERROR_KEY":"errors",
         }
 
 REST_FRAMEWORK = {
