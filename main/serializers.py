212dabd488cc54137984e52427d7f4e0bac0e1d7
izzyjosh <calebayuba222@gmail.com>
2024-02-02 20:57:17 +0100

    Added password field

--- main/serializers.py
+++ main/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    password = serializers.CharField(min_length=8,read_only=True)
+
     class Meta:
         model = User
-        fields = ("url","username","email",)
+        fields = ("url","username","email","password")
+
+    def validate(self,attrs):
+
+        email_exists = User.objects.filter(email=attrs["email"]).exists()
+        if email_exists:
+            raise ValidationError("user with this email already exists")
+        return super().validate(attrs)
